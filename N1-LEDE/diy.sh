d959c11c16d99e6501e5f1d6d3f3470ea02c13b0
x315600 <315601qq@gmail.com>
2025-06-06 19:04:58 +0800

    Update diy.sh

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -25,7 +25,7 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
 git clone --depth=1  https://github.com/gdy666/luci-app-lucky.git package/lucky
 git clone --depth=1 https://github.com/vernesong/OpenClash package/openclash
 git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
-
+mv package/small/luci-app-wechatpush package/luci-app-wechatpush
 
 # iStore
 
@@ -34,11 +34,13 @@ git_sparse_clone main https://github.com/linkease/istore luci
 #添加自定义的软件包源
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-theme-edge
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-lucky
-Remove packages
+
 #删除冲突插件
 #rm -rf $(find ./feeds/packages/net/ -type d -regex ".*\(dns2socks\|geoview\|ipt2socks\|microsocks\|naiveproxy\|shadowsocks-rust\|shadowsocksr-libev\|shadowsocks-libev\|simple-obfs\|tcping\|tuic-client\|v2ray-plugin\|xray-plugin\).*")
 
 #rm -rf $(find ./feeds/luci/ -type d -regex ".*\(passwall\|openclash\|alist\|easymesh\|adguardhome\|lucky\|wechatpush\|ddns-go\|ssr-plus\).*")
+
+
 
 
 
@@ -53,9 +55,26 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/themes/luci-theme-design
 rm -rf feeds/luci/applications/luci-app-design-config
 
-# Default IP
+rm -rf feeds/packages/net/ddns-go
+# rm -rf feeds/packages/net/frp
+# rm -rf feeds/luci/applications/luci-app-frps
+# rm -rf feeds/luci/applications/luci-app-frpc
+rm -rf feeds/packages/net/msd_lite
+# rm -rf feeds/luci/applications/luci-app-wrtbwmon
+# rm -rf feeds/packages/admin/netdata
+# rm -rf feeds/luci/applications/luci-app-netdata
+# rm -rf feeds/packages/net/qBittorrent
+# rm -rf feeds/packages/net/qBittorrent-static
+# rm -rf feeds/luci/applications/luci-app-qbittorrent
+rm -rf feeds/luci/applications/luci-app-serverchan
 
-sed -i 's/192.168.1.1/192.168.2.254/g' package/base-files/files/bin/config_generate
+
+# golang版本修复
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+# Default IP
+sed -i 's/192.168.1.1/192.168.2.254/g' package/base-files/luci2/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.2.254/g' package/base-files/files/bin/config_generate
 
 #修改默认时间格式
 sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $(find ./package/*/autocore/files/ -type f -name "index.htm")

537631f2ea5f5b0de3b9b4aa44dd93233fca7763
root <root@baapqtol.colocrossing.cloud>
2026-06-02 07:16:39 +0800

    feat: align N1 immortalwrt profile

--- N1-IMM/diy.sh
+++ N1-IMM/diy.sh
@@ -2,46 +2,45 @@
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
+  git clone --depth=1 -b "$branch" --single-branch --filter=blob:none --sparse "$repourl"
+  repodir=$(echo "$repourl" | awk -F '/' '{print $(NF)}')
+  cd "$repodir" && git sparse-checkout set "$@"
+  mv -f "$@" ../package
+  cd .. && rm -rf "$repodir"
 }
 
 # Default IP
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 
-
 # Add packages
-#添加科学上网源
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
-#git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
-git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
-#git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
-#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
+# 添加科学上网源
+git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/openwrt-passwall-packages
+git_sparse_clone main https://github.com/Openwrt-Passwall/openwrt-passwall luci-app-passwall
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+git_sparse_clone main https://github.com/nikkinikki-org/OpenWrt-nikki luci-app-nikki nikki mihomo-meta mihomo-alpha
 
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/mosdns
+git_sparse_clone main https://github.com/sbwml/luci-app-diskman luci-app-diskman
+git clone --depth=1 https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
+git_sparse_clone main https://github.com/sirpdboy/luci-app-taskplan luci-app-taskplan
+git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser
 
-#删除库中的插件，使用自定义源中的包。
+# luci-app-filemanager is part of upstream LuCI; pull only the app subtree.
+git clone --depth=1 --filter=blob:none --sparse https://github.com/openwrt/luci package/luci-app-filemanager-src
+cd package/luci-app-filemanager-src
+  git sparse-checkout set applications/luci-app-filemanager
+  mkdir -p ../luci-app-filemanager
+  cp -a applications/luci-app-filemanager/. ../luci-app-filemanager/
+cd .. && rm -rf luci-app-filemanager-src
+
+# 删除库中的插件，使用自定义源中的包。
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
-#rm -rf feeds/luci/applications/luci-app-ddns-go
-#rm -rf feeds/packages/net/ddns-go
-#rm -rf feeds/packages/net/alist
-#rm -rf feeds/luci/applications/luci-app-alist
-#rm -rf feeds/luci/applications/openwrt-passwall
-
-# 替换luci-app-openvpn-server imm源的启动不了服务！
-#rm -rf feeds/luci/applications/luci-app-openvpn-server
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-openvpn-server
-# 调整 openvpn-server 到 VPN 菜单
-#sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/controller/*.lua
-#sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/model/cbi/openvpn-server/*.lua
-#sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/view/openvpn/*.htm
-
-
-#替换luci-app-socat为https://github.com/chenmozhijin/luci-app-socat
-#rm -rf feeds/luci/applications/luci-app-socat
-#git_sparse_clone main https://github.com/chenmozhijin/luci-app-socat luci-app-socat
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/utils/v2dat
